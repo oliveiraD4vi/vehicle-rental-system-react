2f73f56dc1adfbd59ba759f109b24d521ba625ec
Davi Oliveira <davi.comp.oliveira@gmail.com>
2022-08-28 19:33:00 -0300

    [ID-03] header and footer style added

--- src/components/Header/header.js
+++ src/components/Header/header.js
@@ -1,6 +1,13 @@
-import { useNavigate } from 'react-router-dom';
-
+import {
+  Box,
+  Button,
+  BottomNavigation,
+  BottomNavigationAction,
+}
+from '@mui/material';
 import logo from "../../assets/grancars-primary.svg";
+
+import { useNavigate } from 'react-router-dom';
 
 import "./header.css";
 
@@ -10,6 +17,22 @@ const Header = () => {
   return (
     <div className="header-container">
       <img src={logo} alt="brand logo" onClick={() => navigate('/')} />
+
+      <Box className="box-container">
+        <BottomNavigation
+          showLabels
+          onChange={(e, newValue) => {
+            navigate(newValue);
+          }}
+        >
+          <BottomNavigationAction className="navigation-btn" value="/home" label="HOME" />
+          <BottomNavigationAction className="navigation-btn" value="/cars" label="CARROS" />
+          <BottomNavigationAction className="navigation-btn" value="/reservation/form" label="RESERVAS" />
+          <BottomNavigationAction className="navigation-btn" value="/about" label="SOBRE NÓS" />
+        </BottomNavigation>
+      </Box>
+
+      <Button className="primary-button" variant="container">LOGIN</Button>
     </div>
   );
 };
