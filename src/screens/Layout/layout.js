40e217e524539337efb57a7615f6597de3d5f694
Davi Oliveira <davi.comp.oliveira@gmail.com>
2022-09-15 21:22:36 -0300

    [ID-19] added token verification

--- src/screens/Layout/layout.js
+++ src/screens/Layout/layout.js
@@ -1,9 +1,33 @@
+import { useEffect } from "react";
+import { auth } from "../../services/utils";
+import { useNavigate } from "react-router-dom";
+
+import api from "../../services/api";
+import notification from "../../services/notification";
 import Header from "../../components/Header/header";
 import Footer from "../../components/Footer/footer";
 
 import "./layout.css";
 
 const Layout = ({ children }) => {
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    async function checkUser() {
+      if (auth.isAuthenticated()) {
+        try {
+          await api.get("/user/check");
+        } catch (error) {
+          notification("error", "Login expirado");
+          auth.logout();
+          navigate("/");
+        }
+      }
+    }
+
+    checkUser();
+  }, []);
+
   return (
     <div className="layout-container">
       <Header />
